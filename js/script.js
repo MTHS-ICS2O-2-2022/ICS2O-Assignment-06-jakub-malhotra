4c50537405b504b5132522e29309771924a9e66e
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-06-14 00:22:47 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -4,30 +4,24 @@
 // Created on: May 2023
 // This file contains the JS functions for index.html
 
-"use strict"
+"use strict";
 
-const getWeather = async (URLAddress) => {
+const getActivity = async (URLAddress) => {
   try {
-    const result = await fetch(URLAddress)
-    const jsonData = await result.json()
-    console.log(jsonData)
+    const result = await fetch(URLAddress);
+    const jsonData = await result.json();
+    console.log(jsonData);
 
-    const img = jsonData.weather[0].icon
-    document.getElementById("api-image").innerHTML =
-      '<img src="http://openweathermap.org/img/wn/' +
-      img +
-      '@2x.png" alt="weather image">'
+    // Access the activity data
+    const activity = jsonData.activity;
+    const type = jsonData.type;
 
-    const tempInC = jsonData.main.temp - 273.15
-    document.getElementById("tempurature").innerHTML =
-      "<p> Tempurature: " + tempInC.toFixed(1) + " °C</p>"
-
-    document.getElementById("weather").innerHTML =
-      "<p> Weather: " + jsonData.weather[0].main + "</p>"
+    // Update the HTML elements with the retrieved data
+    document.getElementById("activity").innerHTML = "<p>Activity: " + activity + "</p>";
+    document.getElementById("type").innerHTML = "<p>Type: " + type + "</p>";
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
-getWeather(
-  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
-)
+};
+
+getActivity("https://www.boredapi.com/api/activity/");
